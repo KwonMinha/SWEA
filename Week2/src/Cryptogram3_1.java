a4365003064c09b8d951b4880879688f318cdaae
Minha Kwon <minha61@naver.com>
2021-10-27 17:17:57 +0900

    암호문3
    Solved

---
+++ Week2/src/Cryptogram3_1.java
@@ -0,0 +1,73 @@
+/**
+ * @author minha
+ * 2021. 10. 27.
+ * 1230. [S/W 문제해결 기본] 8일차 - 암호문3
+ * 성공 - x를 인덱스로 풀이 
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Cryptogram3_1 {
+	
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		int T = 10;
+
+		for(int test_case = 1; test_case <= T; test_case++) {
+			int N = Integer.parseInt(br.readLine()); // 암호문의 길이 
+			st = new StringTokenizer(br.readLine()); // 암호문 
+			
+			LinkedList<String> cryptoList = new LinkedList<>();
+			
+			for(int i = 0; i < N; i++) {
+				cryptoList.add(st.nextToken());	
+			}
+			
+			N = Integer.parseInt(br.readLine()); // 명령어의 개수 
+			st = new StringTokenizer(br.readLine()); // 명령어  
+			
+			for(int i = 0; i < N; i++) {
+				char command = st.nextToken().charAt(0);
+				
+				if(command == 'I') { // I(삽입) x, y, s : 앞에서부터 x의 위치 바로 다음에 y개의 숫자를 삽입
+					int x = Integer.parseInt(st.nextToken());
+					int y = Integer.parseInt(st.nextToken());
+					for(int j = 0; j < y; j++) {
+						cryptoList.add(x, st.nextToken());
+						x++;
+					}
+				} else if(command == 'D') { // D(삭제) x, y : 앞에서부터 x의 위치 바로 다음부터 y개의 숫자를 삭제
+					int x = Integer.parseInt(st.nextToken());
+					int y = Integer.parseInt(st.nextToken());
+					for(int j = 0; j < y; j++) {
+						if(x + 1 < cryptoList.size()) {
+							cryptoList.remove(x + 1);
+						}
+					}
+				} else if(command == 'A') { // A(추가) y, s : 암호문의 맨 뒤에 y개의 숫자를 덧붙인다. s는 덧붙일 숫자들
+					int y = Integer.parseInt(st.nextToken());
+					for(int j = 0; j < y; j++) {
+						cryptoList.addLast(st.nextToken());
+					}
+				}
+			}
+			
+			sb.append("#" + test_case + " ");
+			
+			for(int i = 0; i < 10; i++) {
+				sb.append(cryptoList.get(i) + " ");
+			}
+			
+			sb.append("\n");
+		}
+
+		System.out.println(sb.toString());
+	}
+}
