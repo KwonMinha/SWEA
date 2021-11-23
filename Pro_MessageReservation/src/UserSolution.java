f49f57edbaaa1ce9941f6d0d2e0b971ae7068606
Minha Kwon <minha61@naver.com>
2021-11-24 03:58:30 +0900

    쪽찌 예약 전송

---
+++ Pro_MessageReservation/src/UserSolution.java
@@ -0,0 +1,35 @@
+class UserSolution
+{
+	private final static int MAXM	= 3;
+
+	public void init(int N)
+	{
+
+	}
+
+	public void sendMessage(int cTimestamp, int uID1, int uID2, int mID, int scheduleTimestamp)
+	{
+
+	}
+
+	public int retrieveSentbox(int cTimestamp, int uID, int mIDList[], int uIDList[], int readList[])
+	{
+		return -1;
+	}
+
+	public int retrieveInbox(int cTimestamp, int uID, int mIDList[], int uIDList[], int readList[])
+	{
+		return -1;
+	}
+
+	public int readMessage(int cTimestamp, int uID, int mID)
+	{
+		return -1;
+	}
+
+	public int deleteMessage(int cTimestamp, int uID, int mID)
+	{
+		return -1;
+	}
+}
+
