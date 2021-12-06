4ebbb9ec793f4e35fd1ac3aeb309928bd3a5286d
Minha Kwon <minha61@naver.com>
2021-12-06 23:50:22 +0900

    아나그램
    Solved

---
+++ Advanced1_AD-HOC/src/UserSolution.java
@@ -0,0 +1,34 @@
+class UserSolution {
+	
+	public static int FindAnagram(int L1, String S1, int L2, String S2) {
+		int[] alpha1 = new int[26];
+		int[] alpha2 = new int[26];
+		
+		for(int i = 0; i < L1; i++)
+			alpha1[S1.charAt(i) - 97]++;
+		
+		for(int i = 0; i < L1; i++)
+			alpha2[S2.charAt(i) - 97]++; 
+		
+		int result = 0;
+		
+		for(int i = 0; i <= L2 - L1; i++) {
+			boolean flag = true; 
+			for(int j = 0; j < 26; j++) {
+				if(alpha2[j] < alpha1[j]) {
+					flag = false;
+					break;
+				}
+			}
+			
+			if(flag) result++;
+			
+			if(i == L2 - L1) break;
+			
+			alpha2[S2.charAt(i)-97]--;
+			alpha2[S2.charAt(i+L1)-97]++;
+		}
+		
+		return result;
+	}
+}
